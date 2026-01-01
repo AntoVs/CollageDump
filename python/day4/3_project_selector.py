76372fb094f4613bd1c1d0d100d05f81f2978b7c
ANTO_VS <147755231+AntoVs@users.noreply.github.com>
2026-01-01 16:05:34 +0530

    3 edit

--- python/day4/3_project_selector.py
+++ python/day4/3_project_selector.py
@@ -1,20 +1,22 @@
-skills = {
-    'cs': {'beginner': 'Basic Web-Based Projects', 'intermediate':'Face Recognition/recognition tracker', 'advanced':'Secure Online Voting System using Blockchain'},
-    'mech': {'beginner': 'Stair-Climbing Trolley', 'intermediate':'E-Bike ', 'advanced':'Health Monitoring System'},
-    'eee': {'beginner': 'Home Automation Using Arduino', 'intermediate':'IoT-Based Smart Energy Meter', 'advanced':'Voice-Controlled Wheelchair'}
-}
+def CourseSelect(a,b):
+    skills = {
+        'cs': {'beginner': 'Web-Based Projects', 'intermediate':'Face Recognition/recognition', 'advanced':'Secure Voting using Blockchain'},
+        'mech': {'beginner': 'Stair-Climbing Trolley', 'intermediate':'E-Bike ', 'advanced':'Health Monitoring System'},
+        'eee': {'beginner': 'Home Automation', 'intermediate':'IoT-Based Energy Meter', 'advanced':'Voice-Controlled Wheelchair'}
+    }
+
+    if a in skills:
+        if b > 8:
+            print(skills[a]['advanced'])
+        elif b > 5:
+            print(skills[a]['intermediate'])
+        elif b > 4:
+            print(skills[a]['beginner'])
+        else:
+            print("improve skills bruh!")
+    else:
+        print("invalid choice!")
 
 a = input("Enter area of interest(cs/mech/eee): ")
 b = int(input("Enter skill level(1-10): "))
-
-if a in skills:
-    if b > 8:
-        print(skills[a]['advanced'])
-    elif b > 5:
-        print(skills[a]['intermediate'])
-    elif b > 4:
-        print(skills[a]['beginner'])
-    else:
-        print("improve skills bruh!")
-else:
-    print("invalid choice!")
+CourseSelect(a,b)
